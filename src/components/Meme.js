e553319097583b3323107b1de9aa3c3696b00989
HannahChen777 <hannahChen.V@gmail.com>
2023-02-21 00:53:21 +0800

    API-fetching-by-useEffect-practice

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import memesData from '../memesData';
+import React, { useEffect, useState } from 'react';
 
 export default function Meme() {
-    let memeArr = memesData.data.memes;
-    let randomNum = Math.floor(Math.random() * memeArr.length);
-    let randomImg = memeArr[randomNum].url;
-    // const [allMemeImg, setAllMemeImg] = React.useState(memesData);
 
-    let [formData, setFormData] = React.useState({
+    const [allMemes, setAllMemes] = useState([]);
+
+    useEffect(function(){
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.data.memes);
+                setAllMemes(data.data.memes)
+            });
+    }, [])
+
+    function getMemeImg(){
+        let randomNum = Math.floor(Math.random() * allMemes.length);
+        let randomImg = allMemes[randomNum].url;
+        setFormData(previousValue => ({
+            ...previousValue,
+            randomImg: randomImg
+        })
+    )}
+
+    let [formData, setFormData] = useState({
         topText: '',
         bottomText: '',
-        randomImg: randomImg
+        randomImg: "http://i.imgflip.com/1bij.jpg"
     });
 
     function handleFormData(event){
@@ -22,15 +37,6 @@ export default function Meme() {
     }
     
     console.log(formData);
-
-    const [meme, setMeme] = React.useState(memesData);
-
-    function getMemeImg(){
-        setMeme(previousValue => ({
-            ...previousValue,
-            randomImg: randomImg
-        })
-    )}
 
     return (
         <main>
@@ -56,7 +62,7 @@ export default function Meme() {
                     />
                     <button onClick={getMemeImg} className="meme-button">Get my new meme image</button>
                     <div className="meme">
-                        <img className="meme-img" src={meme.randomImg} />
+                        <img className="meme-img" src={formData.randomImg} />
                         <h2 className="meme-text top">{formData.topText}</h2>
                         <h2 className="meme-text bottom">{formData.bottomText}</h2>
                     </div>
@@ -64,27 +70,113 @@ export default function Meme() {
             </section>
         </main>
     )
-
-    /*
-    function greeting(name){
-        let date = new Date();
-        let hour = date.getHours();
-        let timeOfDay;
-        if ( hour >= 4 && hour < 12){
-            timeOfDay = 'morning';
-        } else if ( hour >= 12 && hour < 17){
-            timeOfDay = 'afternoon';
-        } else if ( hour >= 17 && hour < 20 ){
-            timeOfDay = 'evening';
-        } else if ( hour >= 20 || hour < 4 ){
-            timeOfDay = 'night';
-        }
-        return `Good ${timeOfDay}, ${name}!`;
-    }
     
-    greeting('Hannah');
-    */
+        /*
+        function greeting(name){
+            let date = new Date();
+            let hour = date.getHours();
+            let timeOfDay;
+            if ( hour >= 4 && hour < 12){
+                timeOfDay = 'morning';
+            } else if ( hour >= 12 && hour < 17){
+                timeOfDay = 'afternoon';
+            } else if ( hour >= 17 && hour < 20 ){
+                timeOfDay = 'evening';
+            } else if ( hour >= 20 || hour < 4 ){
+                timeOfDay = 'night';
+            }
+            return `Good ${timeOfDay}, ${name}!`;
+        }
+        
+        greeting('Hannah');
+        */
 }
+
+
+// export default function Meme() {
+//     let memeArr = memesData.data.memes;
+//     let randomNum = Math.floor(Math.random() * memeArr.length);
+//     let randomImg = memeArr[randomNum].url;
+//     // const [allMemeImg, setAllMemeImg] = React.useState(memesData);
+
+//     let [formData, setFormData] = React.useState({
+//         topText: '',
+//         bottomText: '',
+//         randomImg: randomImg
+//     });
+
+//     function handleFormData(event){
+//         const {name, value} = event.target; 
+//         setFormData(previousValue => ({
+//             ...previousValue,
+//             [name]: value
+//         }))
+//     }
+    
+//     console.log(formData);
+
+//     const [meme, setMeme] = React.useState(memesData);
+
+//     function getMemeImg(){
+//         setMeme(previousValue => ({
+//             ...previousValue,
+//             randomImg: randomImg
+//         })
+//     )}
+
+//     return (
+//         <main>
+//             <section className="meme-main">
+//                 {/* <form>with<button>will really submit data from input till you stop</form> */}
+//                 <div className="meme-form">
+//                     <input 
+//                         type="text"
+//                         placeholder="Top Text"
+//                         className="meme-input" 
+//                         name="topText"
+//                         onChange={handleFormData}
+//                         value={formData.topText}
+                        
+//                     />
+//                     <input 
+//                         type="text" 
+//                         placeholder="Button Text"
+//                         className="meme-input" 
+//                         name="bottomText"
+//                         onChange={handleFormData}
+//                         value={formData.bottomText}
+//                     />
+//                     <button onClick={getMemeImg} className="meme-button">Get my new meme image</button>
+//                     <div className="meme">
+//                         <img className="meme-img" src={meme.randomImg} />
+//                         <h2 className="meme-text top">{formData.topText}</h2>
+//                         <h2 className="meme-text bottom">{formData.bottomText}</h2>
+//                     </div>
+//                 </div>
+//             </section>
+//         </main>
+//     )
+
+//     /*
+//     function greeting(name){
+//         let date = new Date();
+//         let hour = date.getHours();
+//         let timeOfDay;
+//         if ( hour >= 4 && hour < 12){
+//             timeOfDay = 'morning';
+//         } else if ( hour >= 12 && hour < 17){
+//             timeOfDay = 'afternoon';
+//         } else if ( hour >= 17 && hour < 20 ){
+//             timeOfDay = 'evening';
+//         } else if ( hour >= 20 || hour < 4 ){
+//             timeOfDay = 'night';
+//         }
+//         return `Good ${timeOfDay}, ${name}!`;
+//     }
+    
+//     greeting('Hannah');
+//     */
+// }
 
 //onMouseOver vs onMouseEnter
 //there's a bit different in children element
